ec841bdda3aef48d2664ecad470d74037babc861
Jesse Young <young295@purdue.edu>
2019-07-12 18:54:17 -0400

    implment data fetching logic - fetch data from existing json uri - reformat data to be more readable and usable - create data file locally to project

---
+++ src/format-data.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const fetch = require('node-fetch')
+const emojiURI = 'https://unpkg.com/emoji.json@1.2.0/emoji.json'
+
+const formatEmojiData = async uri => {
+  const origEmojiJson = await fetch(uri).then(response => response.json())
+
+  // TODO:
+  // 1. remove 'default'
+  // 2. remove char from the end of date values
+  // 3. reformat keywords from string to arr
+  // 4. remove repetition from name values and reformat name to lowercase
+
+  origEmojiJson.map((emojiObj, i) => {
+    emojiObj.keywords = emojiObj.keywords.split(` | `)
+    emojiObj.name = emojiObj.name.split(`\n`)[0].toLowerCase()
+    emojiObj.date = emojiObj.date.slice(0, -1)
+    delete emojiObj.default
+  })
+  return { ...origEmojiJson }
+}
+
+formatEmojiData(emojiURI).then(formattedEmojiData => {
+  fs.writeFile('./data/emoji-data.json', JSON.stringify(formattedEmojiData), err => {
+    if (err) return err
+  })
+})
