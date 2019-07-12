96d042dbbefdfa2e5f603009125208e24aa7f10a
Jesse Young <young295@purdue.edu>
2019-07-12 19:33:26 -0400

    update logic to add flag names to respective keywords arrays

--- src/format-data.js
+++ src/format-data.js
@@ -10,10 +10,16 @@ const formatEmojiData = async uri => {
   // 2. remove char from the end of date values
   // 3. reformat keywords from string to arr
   // 4. remove repetition from name values and reformat name to lowercase
+  // 5. for flags add name of flag to keyword array
 
   origEmojiJson.map((emojiObj, i) => {
     emojiObj.keywords = emojiObj.keywords.split(` | `)
     emojiObj.name = emojiObj.name.split(`\n`)[0].toLowerCase()
+    if (emojiObj.keywords.includes(`flag`)) {
+      let flagNameArr = emojiObj.name.split(' ')
+      emojiObj.keywords = [...emojiObj.keywords, ...flagNameArr]
+      emojiObj.keywords.push(emojiObj.name)
+    }
     emojiObj.date = emojiObj.date.slice(0, -1)
     delete emojiObj.default
   })
